245f3e0031949854186a1d5379b7406f002a25a0
farazqureshi999 <blackhat786@hotmail.com>
2020-01-29 17:47:55 +0500

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,118 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatDialogModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatChipsModule,
+  MatListModule,
+  MatTooltipModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatSortModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatRadioModule,
+  MatBadgeModule,
+  MatToolbar,
+  MatToolbarRow,
+  MatIconRegistry
+  } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatListModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatGridListModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatBadgeModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatListModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatGridListModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatBadgeModule
+  ]
+})
+export class MaterialModule {
+  constructor(iconRegistery: MatIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistery.addSvgIcon('fb', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/fb.svg'));
+        iconRegistery.addSvgIcon('linkedin', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/linkedin.svg'));
+        iconRegistery.addSvgIcon('git', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
+  }
+}
