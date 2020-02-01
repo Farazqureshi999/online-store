2bd7f89fb2ff653eab840b342806d91de70f835a
farazqureshi999 <blackhat786@hotmail.com>
2020-02-02 01:53:22 +0500

    set product

--- src/app/admin/setproduct/setproduct.component.ts
+++ src/app/admin/setproduct/setproduct.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetproductComponent implements OnInit {
   toggleField: string;
+  savedChanges = false;
   constructor() { }
 
   ngOnInit() {
@@ -20,5 +21,17 @@ export class SetproductComponent implements OnInit {
         filter = filter;
     }
     this.toggleField = filter;
+  }
+
+  getFilterData() {
+
+  }
+
+  getData() {
+
+  }
+
+  setData() {
+
   }
 }
